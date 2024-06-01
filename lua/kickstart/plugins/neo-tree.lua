21f86c6703dc655d81210ec7a744de08d1c5dba9
Sandeep Bhutani <jarvis@LAPTOP-U49NOPSD>
2024-06-01 08:26:08 +0530

    new changes

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -15,6 +15,17 @@ return {
   },
   opts = {
     filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          -- '.git',
+          -- '.DS_Store',
+          -- 'thumbs.db',
+        },
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
