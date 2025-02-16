d4dded109111ad8ec7e82ff28f2911fb38361839
Sandeep Bhutani <jarvis@Sandeeps-MacBook-Pro.local>
2025-02-16 17:33:22 +0530

    added more config

--- init.lua
+++ init.lua
@@ -633,7 +633,16 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        pyright = {},
+        pyright = {
+          settings = {
+            python = {
+              analysis = {
+                autoSearchPaths = true,
+                useLibraryCodeForTypes = true,
+              },
+            },
+          },
+        },
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
